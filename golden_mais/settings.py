6db0dba6200904d75605ee30e69e1084f1c316aa
Rose Ann <sillarroseann7@gmail.com>
2025-11-22 15:55:25 +0800

    Render YAML add

--- golden_mais/settings.py
+++ golden_mais/settings.py
@@ -9,12 +9,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
+SECRET_KEY = os.getenv('SECRET_KEY', '2db2f7fb6507b474c20bc0c5b1664c70')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 INSTALLED_APPS = [
