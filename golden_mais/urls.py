a666b1a614f5a4c7a1f32cc053a6a319c7ea6b95
Rose Ann <sillarroseann7@gmail.com>
2025-11-24 19:20:13 +0800

    Add sitemap.xml and robots.txt for SEO

--- golden_mais/urls.py
+++ golden_mais/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path('', include('core.urls')),
     # Google Search Console verification
     path('google4d1a4abd9adf2e93.html', TemplateView.as_view(template_name='google4d1a4abd9adf2e93.html', content_type='text/html')),
+    # Sitemap and robots
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
 
 if settings.DEBUG:
